c20041b2cc2ff70f260f01d14093bc6afe26ffd9
nothingChen <780426942@qq.com>
2014-12-03 23:20:50 +0800

    wqefrg

--- TestGitTeam/src/com/czy/TestB.java
+++ TestGitTeam/src/com/czy/TestB.java
@@ -2,7 +2,7 @@ package com.czy;
 
 public class TestB {
 	// this is TestB by
-	// this is TestB by nothing chenasdasd
-	// this is TestB by nothing chenasdasd
+	// this is TestB by nqfwewgqewgbq3ebnqeothing chenasdasd
+	// this is TestadsfvbaebB by nothing chenasdasd
 
 }
